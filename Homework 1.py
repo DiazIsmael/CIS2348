e8203a9a29aa47ccb6888d9b1ab68941efcc1ed7
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-09-17 20:11:14 -0500

    testing file push

--- Homework 1.py
+++ Homework 1.py
@@ -1,6 +1,12 @@
 #Ismael Diaz (PSID: 1846093)
 #CIS 2348 Homework #1
 
-bday = 0
-bmonth = 0
+bday = int
+bmonth = int
+byear = int
+
+print("Welcome to your Birthday Calculator!")
+bday = input("Please enter the day of your birth:")
+bmonth = input("Birth month:")
+byear = input("Birth year:")
 
