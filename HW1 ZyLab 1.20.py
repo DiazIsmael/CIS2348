90cbd037931b0e292a5d744a6c128a580f9a15b6
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-09-17 22:02:28 -0500

    puuuush

---
+++ HW1 ZyLab 1.20.py
@@ -0,0 +1,11 @@
+#Ismael Diaz (PSID: 1846093)
+#CIS 2348 Homework #1 Zylabs 1.20
+
+user_num = int(input("Enter integer: "))
+print("You entered: ", user_num)
+print(user_num, "squared is ", user_num * user_num)
+print("And ", user_num, "cubed is ", user_num * user_num * user_num "!!")
+
+user_num1 = int(input("Enter another integer: "))
+print(user_num, " + ", user_num1, " is ", user_num + user_num1)
+print(user_num, " * ", user_num1, " is ", user_num * user_num1)
