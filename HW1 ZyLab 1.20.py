4c9836ae97cda58f3550229436eee2ef990b269f
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-09-17 22:06:10 -0500

    testing file push

--- HW1 ZyLab 1.20.py
+++ HW1 ZyLab 1.20.py
@@ -4,7 +4,7 @@
 user_num = int(input("Enter integer: "))
 print("You entered: ", user_num)
 print(user_num, "squared is ", user_num * user_num)
-print("And ", user_num, "cubed is ", user_num * user_num * user_num "!!")
+print("And ", user_num, "cubed is ", user_num * user_num * user_num, "!!")
 
 user_num1 = int(input("Enter another integer: "))
 print(user_num, " + ", user_num1, " is ", user_num + user_num1)
