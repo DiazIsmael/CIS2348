1ef2e6c55a70733db4c7933e80babd19cb702cd1
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-09-18 22:03:10 -0500

    testing file push

--- HW1 ZyLab 2.19.py
+++ HW1 ZyLab 2.19.py
@@ -1,5 +1,5 @@
 #Ismael Diaz (PSID: 1846093)
-#CIS 2348 Homework #1 Zylabs 1.20
+#CIS 2348 Homework #1 Zylabs 2.19
 
 ljuice = float(input("Enter amount of lemon juice (in cups):\n"))
 water = float(input("Enter amount of water (in cups):\n"))
