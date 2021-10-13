b371bf615d96209f7a409b5967b45898a35dc764
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-10-13 12:08:03 -0500

    creating ZyLab 6.17 file

--- Homework 2/HW2 ZyLab 6.17.py
+++ Homework 2/HW2 ZyLab 6.17.py
@@ -1,4 +1,22 @@
 #Ismael Diaz (PSID: 1846093)
 #CIS 2348 Homework #2 Zylabs 6.17
 
-code
+rawpassword = input()
+newpassword = ""
+
+for char in rawpassword:
+    if char == "i":
+        newpassword = newpassword + "!"
+    elif char == "a":
+        newpassword = newpassword + "@"
+    elif char == "m":
+        newpassword = newpassword + "M"
+    elif char == "B":
+        newpassword = newpassword + "8"
+    elif char == "o":
+        newpassword = newpassword + "."
+    else:
+        newpassword = newpassword + char
+newpassword = newpassword + "q*s"
+
+print(newpassword)
