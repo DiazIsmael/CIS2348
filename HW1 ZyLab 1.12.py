7d050baa2a4733932dd0eb8840986ebb49b59358
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-10-12 22:23:13 -0500

    new file

--- HW1 ZyLab 1.12.py
+++
@@ -1,7 +0,0 @@
-#Ismael Diaz (PSID: 1846093)
-#CIS 2348 Homework #1 Zylabs 1.12
-
-userNum = int(input())
-userNumSquared = userNum * userNum
-
-print(userNumSquared)
--- HW2 ZyLab 6.17.py
+++
@@ -1,3 +0,0 @@
-#Ismael Diaz (PSID: 1846093)
-#CIS 2348 Homework #2 Zylabs 6.17
-
