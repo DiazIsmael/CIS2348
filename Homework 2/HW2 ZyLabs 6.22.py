aeb700f7f1f6766fd3799a976220051482389ca2
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-10-13 12:43:01 -0500

    completed ZyLabs 6.22

--- Homework 2/HW2 ZyLabs 6.22.py
+++ Homework 2/HW2 ZyLabs 6.22.py
@@ -1,3 +1,22 @@
 #Ismael Diaz (PSID: 1846093)
 #CIS 2348 Homework #2 Zylabs 6.22
 
+x1 = int(input())
+y1 = int(input())
+ans1 = int(input())
+x2 = int(input())
+y2 = int(input())
+ans2 = int(input())
+
+xsolution = int
+ysolution = int
+
+for x in range(-10, 11):
+    for y in range(-10, 11):
+        if (x1 * x) + (y1 * y) == ans1 and (x2 * x) + (y2 * y) == ans2:
+            xsolution = x
+            ysolution = y
+            print(xsolution, ysolution)
+            exit()
+
+print("No Solution")
