58be8a276d15a6e3432fc44dbdac5a8392c926ad
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-10-13 20:01:43 -0500

    Homework 2 Part A Complete

--- Homework 2/HW2.py
+++ Homework 2/HW2.py
@@ -1,2 +1,21 @@
 #Ismael Diaz (PSID: 1846093)
-#CIS 2348 Homework #2 Zylabs 6.17
+#CIS 2348 Homework #2
+from datetime import date
+
+months_dict = {"January": 1, "February": 2, "March": 3, "April": 4, "May": 5, "June": 6, "July": 7, "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
+todaysdate = date.today()
+userdate = date(1111, 11, 1)
+rawdate = ""
+
+while rawdate != "-1":
+    rawdate = input("Enter date in format \"March 1, 1990\": ")
+
+    x = rawdate.replace(",", "")
+    x = x.split(" ")
+
+    if x[0] in months_dict.keys() and 0 < int(x[1]) < 32 and len(x[2]) == 4:
+        userdate = date(int(x[2]), int(months_dict[x[0]]), int(x[1]))
+        if userdate < todaysdate:
+            print("{}/{}/{}".format(months_dict[x[0]], x[1], x[2]))
+        else:
+            pass
