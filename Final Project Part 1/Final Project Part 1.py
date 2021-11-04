37cbabf5738e7a5c5c4ba8088f2f0a8a0d2c455d
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-11-04 08:09:31 -0500

    almost nearly so close to getting it

--- Final Project Part 1/Final Project Part 1.py
+++ Final Project Part 1/Final Project Part 1.py
@@ -1,4 +1,69 @@
 #Ismael Diaz (PSID: 1846093)
 #CIS 2348 Final Project Pt. 1
-
 from datetime import date
+from datetime import datetime
+import csv
+from fileinput import close
+
+inventory = {}
+todaysdate = date.today()
+
+#populating the master inventory dictionary
+inputFile = open('ManufacturerList.csv')
+reader = csv.reader(inputFile)
+
+for item in reader:
+    inventory[item[0]] = [item[1], item[2]]
+
+inputFile.close()
+
+#importing prices and appending to inventory dictionary
+inputFile = open('PriceList.csv')
+reader = csv.reader(inputFile)
+
+for item in reader:
+    inventory[item[0]].append(item[1])
+
+inputFile.close()
+
+#importing service dates and appending to inventory dict
+inputFile = open('ServiceDatesList.csv')
+reader = csv.reader(inputFile)
+
+for item in reader:
+    inventory[item[0]].append(item[1])
+
+inputFile.close()
+
+#appending damaged attribute to the end of list
+inputFile = open('ManufacturerList.csv')
+reader = csv.reader(inputFile)
+
+for item in reader:
+    inventory[item[0]].append(item[3])
+
+inputFile.close()
+
+#sorting and writing to fullinventory.csv
+outputFile = open('FullInventory.csv', 'w')
+writer = csv.writer(outputFile)
+
+sortedInventory = {}
+sortedList = sorted(inventory.values())
+
+for list in sortedList:
+    for key in inventory.keys():
+        if inventory[key] == list:
+            sortedInventory[key] = inventory[key]
+
+writer.writerow(sortedInventory.items())
+print(sortedInventory)
+
+"""outputList = []
+
+for keys in sortedInventory.keys():
+    outputList.append(keys)
+    for values in sortedInventory.values():
+        outputList.append(sortedInventory[keys])
+
+print(outputList)"""
