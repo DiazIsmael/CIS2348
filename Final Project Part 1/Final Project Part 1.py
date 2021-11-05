f54cd2d13d9fbae7534d67b714711a799c73c2a4
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-11-04 23:29:53 -0500

    took an entire sleepless night but PART A COMPLETE

--- Final Project Part 1/Final Project Part 1.py
+++ Final Project Part 1/Final Project Part 1.py
@@ -5,52 +5,69 @@ from datetime import datetime
 import csv
 from fileinput import close
 
-inventory = {}
+inventory = []
 todaysdate = date.today()
 
-#populating the master inventory dictionary
+#populating the master inventory
 inputFile = open('ManufacturerList.csv')
 reader = csv.reader(inputFile)
 
-for item in reader:
-    inventory[item[0]] = [item[1], item[2]]
+for items in reader:
+    i = 0
+    inventory.insert(i, [items[0], items[1], items[2]])
+    i += 1
 
 inputFile.close()
 
-#importing prices and appending to inventory dictionary
+#importing prices and appending to inventory
 inputFile = open('PriceList.csv')
 reader = csv.reader(inputFile)
 
-for item in reader:
-    inventory[item[0]].append(item[1])
+for prices in reader:
+    for list in inventory:
+        i = 0
+        if prices[0] == list[0]:
+            list.append(prices[1])
+        i += 1
 
 inputFile.close()
 
-#importing service dates and appending to inventory dict
+#importing service dates and appending to inventory
 inputFile = open('ServiceDatesList.csv')
 reader = csv.reader(inputFile)
 
-for item in reader:
-    inventory[item[0]].append(item[1])
+for dates in reader:
+    for list in inventory:
+        i = 0
+        if dates[0] == list[0]:
+            list.append(dates[1])
+        i += 1
 
 inputFile.close()
 
-#appending damaged attribute to the end of list
+#appending damaged attribute to the end of list for correct csv writing order
 inputFile = open('ManufacturerList.csv')
 reader = csv.reader(inputFile)
 
-for item in reader:
-    inventory[item[0]].append(item[3])
+for damaged in reader:
+    for list in inventory:
+        i = 0
+        if damaged[0] == list[0]:
+            list.append(damaged[3])
+        i += 1
 
 inputFile.close()
 
 #sorting and writing to fullinventory.csv
-outputFile = open('FullInventory.csv', 'w')
+outputFile = open('FullInventory.csv', 'w', newline='')
 writer = csv.writer(outputFile)
 
-sortedInventory = {}
-sortedList = sorted(inventory.values())
+alphaInventory = sorted(inventory, key=lambda x: x[1])
+writer.writerows(alphaInventory)
 
+outputFile.close()
+
+'''
 for list in sortedList:
     for key in inventory.keys():
         if inventory[key] == list:
@@ -67,3 +84,5 @@ for keys in sortedInventory.keys():
         outputList.append(sortedInventory[keys])
 
 print(outputList)"""
+
+'''
