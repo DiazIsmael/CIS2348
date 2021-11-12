32c5ab4cec320090ba1ca6f063bcf73523e39470
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-11-12 04:09:51 -0600

    shopping cart part 1 complete

--- Homework 3/ZyLabs 10.17.py
+++ Homework 3/ZyLabs 10.17.py
@@ -1,2 +1,33 @@
 #Ismael Diaz (PSID: 1846093)
 #CIS 2348 Homework #3 Zylabs 10.17
+
+class ItemToPurchase:
+    def __init__(self, item_name="none", item_price=0.00, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+
+    def print_item_cost(self):
+        print("{} {} @ ${:.0f} = ${:.0f}".format(self.item_name, self.item_quantity, self.item_price, (self.item_price*self.item_quantity)))
+
+
+if __name__ == "__main__":
+    print("Item 1")
+    name = input("Enter the item name:\n")
+    price = float(input("Enter the item price:\n"))
+    qty = int(input("Enter the item quantity:\n"))
+    item1 = ItemToPurchase(name, price, qty)
+    print()
+
+    print("Item 2")
+    name = input("Enter the item name:\n")
+    price = float(input("Enter the item price:\n"))
+    qty = int(input("Enter the item quantity:\n"))
+    item2 = ItemToPurchase(name, price, qty)
+    print()
+
+    print("TOTAL COST")
+    item1.print_item_cost()
+    item2.print_item_cost()
+    print()
+    print("Total: ${:.0f}".format((item1.item_price*item1.item_quantity) + (item2.item_price*item2.item_quantity)))
