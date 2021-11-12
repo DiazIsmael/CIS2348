f80d6befd071ebcc8d948c0baccc3b9d5a24a84e
Ismael <89555526+DiazIsmael@users.noreply.github.com>
2021-11-12 02:53:45 -0600

    committing..

---
+++ Homework 3/ZyLabs 10.11.py
@@ -0,0 +1,36 @@
+#Ismael Diaz (PSID: 1846093)
+#CIS 2348 Homework #3 Zylabs 10.11
+
+class FoodItem:
+    def __init__(self, name='None', fat=0.0, carbs=0.0, protein=0.0):
+        self.name = name
+        self.fat = fat
+        self.carbs = carbs
+        self.protein = protein
+
+    def get_calories(self, num_servings):
+        # Calorie formula
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings
+        return calories
+
+    def print_info(self):
+        print('Nutritional information per serving of {}:'.format(self.name))
+        print('   Fat: {:.2f} g'.format(self.fat))
+        print('   Carbohydrates: {:.2f} g'.format(self.carbs))
+        print('   Protein: {:.2f} g'.format(self.protein))
+
+if __name__ == "__main__":
+    name = input()
+    fat = float(input())
+    carbs = float(input())
+    protein = float(input())
+    servings = float(input())
+
+    fooditem = FoodItem()
+    fooditem.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(servings, fooditem.get_calories(servings)))
+    print()
+
+    fooditem = FoodItem(name, fat, carbs, protein)
+    fooditem.print_info()
+    print('Number of calories for {:.2f} serving(s): {:.2f}'.format(servings, fooditem.get_calories(servings)))
---
+++ Homework 3/ZyLabs 10.15.py
@@ -0,0 +1,3 @@
+#Ismael Diaz (PSID: 1846093)
+#CIS 2348 Homework #3 Zylabs 10.11
+
